fb358ffd044d92052f19982816553d1b2b007e38
hurricane-ch <boev_@abv.bg>
2023-10-11 12:59:46 +0300

    Some fixes

--- src/main/java/org/softuni/mobilele/model/entity/BrandEntity.java
+++ src/main/java/org/softuni/mobilele/model/entity/BrandEntity.java
@@ -12,6 +12,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
